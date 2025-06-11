5f4b1ac2a42d0a48dcb6a7ac4c8407dc0e264bc8
egamasa <40469431+egamasa@users.noreply.github.com>
2025-06-12 00:18:01 +0900

    lambda-radiko / 関数名見直し

--- lambda-radiko/radiko_download/app.rb
+++ lambda-radiko/radiko_download/app.rb
@@ -102,7 +102,7 @@ def upload_to_s3(file_path, file_name)
 end
 
 def main(event, context)
-  logger = Logger.new($stdout, progname: 'radikoDownload')
+  logger = Logger.new($stdout, progname: 'RadikoDownload')
   logger.formatter =
     proc do |severity, datetime, progname, msg|
       log = {
