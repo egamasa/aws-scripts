1fa709f12eec5191462b79a93f9c80d87a1f2af2
egamasa <40469431+egamasa@users.noreply.github.com>
2026-08-12 13:14:40 +0900

    lambdiko / refs #35 radiko 日時を29時間制で出力

--- functions/lambdiko/radiko-download/app.rb
+++ functions/lambdiko/radiko-download/app.rb
@@ -81,6 +81,38 @@ def download_segments(urls, file_dir)
   segment_file_path_list.compact
 end
 
+def format_airtime(ft_str, to_str)
+  ft = to_time(ft_str)
+  to = to_time(to_str)
+
+  date = ft.to_date
+  ft_hour = ft.hour
+  to_hour = to.hour
+
+  # 放送中に日付を跨ぐ番組
+  # 終了時刻のみ29時間制表記
+  to_hour += 24 if to.hour < ft.hour
+
+  # 深夜0〜4時台に開始する番組
+  if ft.hour < 5
+    # 放送日は前日
+    date -= 1
+    # 開始時刻・終了時刻を29時間制表記
+    ft_hour += 24
+    to_hour += 24
+  end
+
+  ft_hh = ft_hour.to_s.rjust(2, '0')
+  ft_mm = ft.strftime('%M')
+  to_hh = to_hour.to_s.rjust(2, '0')
+  to_mm = to.strftime('%M')
+
+  {
+    file_name: "#{date.strftime('%Y%m%d')}#{ft_hh}#{ft_mm}",
+    notify: "#{date.strftime('%Y-%m-%d')} #{ft_hh}:#{ft_mm}-#{to_hh}:#{to_mm}"
+  }
+end
+
 def parse_metadata_date(date_str)
   return nil if date_str.nil? || date_str.empty?
   Time.parse(date_str).strftime('%Y-%m-%d')
@@ -200,7 +232,9 @@ def main(event, context)
 
     raise 'Segment count mismatch' unless segment_urls.count == segment_file_path_list.count
 
-    output_file_name = "#{event['title']}_#{event['station_id']}_#{event['ft'][0...12]}.m4a"
+    airtime = format_airtime(event['ft'], event['to'])
+
+    output_file_name = "#{event['title']}_#{event['station_id']}_#{airtime[:file_name]}.m4a"
     output_file_path = "#{file_dir}/#{output_file_name}"
 
     metadata_options = build_metadata_options(event['metadata'])
@@ -231,12 +265,7 @@ def main(event, context)
 
     fields = [
       { name: 'Title', value: event['metadata']['title'], inline: false },
-      {
-        name: 'On Air',
-        value:
-          "#{parse_metadata_date(event['ft'][0..7])} #{event['ft'][8..9]}:#{event['ft'][10..11]}-#{event['to'][8..9]}:#{event['to'][10..11]}",
-        inline: true
-      },
+      { name: 'On Air', value: airtime[:notify], inline: true },
       { name: 'Size', value: file_size, inline: true }
     ]
     send_notify(status: :ok, description: s3_file_path, fields: fields)
